f836a65f42e25bcbced1c80c2a51180c468622e0
DamianoCelesti <damiano.celesti@gmail.com>
2025-03-10 15:53:14 +0100

    feat show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -16,7 +16,28 @@ function index(req, res) {
 
 //show
 function show(req, res) {
+    const id = parseInt(req.params.id);
 
+    // Query per ottenere il movie specifico
+    const detailMovie = 'SELECT * FROM movies WHERE id = ?';
+
+    const reviewsMovie = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    connection.query(detailMovie, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+
+        if (movieResults.length === 0) return res.status(404).json({ error: 'Movies not found' });
+
+        // res.json(results[0]); // Restituiamo solo il movie specifico
+        const movie = movieResults[0];
+        connection.query(reviewsMovie, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+            // Aggiorno l'oggetto movie con le review
+            movie.reviews = reviewsResults;
+            // ritorniamo l'oggetto completo
+            res.json(movie);
+        });
+    });
 }
 
 
--- data/db.js
+++ data/db.js
@@ -1,12 +1,19 @@
+// Importo il modulo per poter gestire MySQL
 const mysql = require('mysql2');
+
+// Creo una connessione al db usando le variabili d'ambiente
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
+// avvio la connessione al db
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL!');
 });
+
+// esporto
 module.exports = connection;
